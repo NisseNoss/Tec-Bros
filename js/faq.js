2d834917faf40679e6fe6eb61ea60ca48bc00734
NonStop-png <e-elivaa1@asvg.no>
2021-09-30 20:07:51 +0200

    fwe

--- js/faq.js
+++ js/faq.js
@@ -1,6 +1,43 @@
 fetch("../json/faq.json")
-    .then(response => response.json)
-    .then(data=>
+    .then(response => response.json())
+    .then(data =>
     {
-        console.log(data)
-    })
+        for(const question of data)
+        {
+            document.querySelector("#FAQ").innerHTML+=`
+                <div>
+                    <h3>${question.question}</h3>
+                    <p>${question.answer}</p>
+                </div>
+            `;
+        }
+    }).then(() =>
+    {
+        document.querySelectorAll("#FAQ h3").forEach(function (e, index)
+        {
+            setTimeout(function ()
+            {
+                e.style.opacity = "1";
+
+                const next = e.nextElementSibling;
+                next.style.width = `${e.offsetWidth - 100}px`;
+                next.style.marginBottom = `-${next.offsetHeight}px`;
+
+                e.addEventListener("click", function ()
+                {
+                    if (next.style.transform === "none")
+                    {
+                        next.style.transform = "translateY(-100%)";
+                        next.style.marginBottom = `-${next.offsetHeight}px`;
+                        next.style.opacity = "0";
+                    }
+                    else
+                    {
+                        next.style.transform = "none";
+                        next.style.marginBottom = "0";
+                        next.style.opacity = "1";
+                    }
+                });
+            }, index * 30);
+        });
+    });
